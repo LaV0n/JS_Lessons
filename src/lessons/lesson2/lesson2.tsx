c0f0f17eb8ed29aa89ca010bfb01b33182c96cff
LaVon <leonidmedica@gmail.com>
2022-07-27 13:24:44 +0200

    Initial commit

---
+++ src/lessons/lesson2/lesson2.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+
+
+console.log('lesson 2');
+
+// Lexical environment
+// http://jsflow.org/docs/lex-env/
+
+//// Closure
+// https://learn.javascript.ru/closure
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
+// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-%D1%80%D0%B0%D0%B7-%D0%B8-%D0%BD%D0%B0%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-c211805b6898
+// https://www.youtube.com/watch?v=pahO5XjnfLA
+
+//// Сurrying
+// https://learn.javascript.ru/currying-partials
+// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%BA%D0%B0%D1%80%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2-javascript-5ec4a1d88827
+
+// Pattern Module
+// https://habr.com/ru/company/ruvds/blog/419997/
+
+// Recursion
+// https://learn.javascript.ru/recursion
+// https://www.youtube.com/watch?v=Kuq6oIN3PH0
+
+
+// Task 01
+// Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+function sum(x: number) {
+    function subsum(y: number) {
+        return x + y
+    }
+
+    return subsum
+}
+
+console.log(sum(3)(6))
+
+// Task 02
+// Реализовать функцию makeCounter которая работает следующим образом:
+// const counter = makeCounter();
+// counter(); // 1
+// counter(); // 2
+// const counter2 = makeCounter();
+// counter2(); // 1
+// counter(); // 3
+
+const makeCounter = () => {
+    let count = 0;
+    return () => {
+        return count++
+    }
+}
+let counter = makeCounter();
+const counter2 = makeCounter();
+console.log("T1 counter : " + counter())
+console.log("T1 counter : " + counter())
+console.log("T1 counter2 : " + counter2())
+console.log("T1 counter : " + counter())
+
+// Task 03
+// Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
+// и возвращала следующий объект методов:
+// increase: +1
+// decrease: -1
+// reset: установить счетчик в 0;
+// set: установить счетчик в заданное значение;
+
+const newCounter = (x: number) => {
+    let count = x;
+    return {
+        increase: () => {
+            return () => {
+                return ++count
+            }
+        },
+        decrease: () => {
+            return () => {
+                return --count
+            }
+        },
+        reset:  () => {
+            return () => {
+                count = x;
+                return count
+            }
+        },
+        set: (y: number) => {
+            return ()=>{
+                count = y;
+                return count
+            }
+        }
+    };
+}
+let num = newCounter(2).increase();
+console.log("2 ++ : " + num());
+console.log("2 ++ : " + num());
+console.log("2 ++ : " + num());
+let num2 = newCounter(5).increase();
+console.log("5 ++ : " + num2());
+console.log("5 ++ : " + num2());
+console.log("5 ++ : " + num2());
+let num3 = newCounter(2).decrease();
+console.log("2 -- : " + num3());
+console.log("2 -- : " + num3());
+console.log("2 -- : " + num3());
+let num4 = newCounter(5).decrease();
+console.log("5 -- : " + num4());
+console.log("5 -- : " + num4());
+console.log("5 -- : " + num4());
+let num5= newCounter(10);
+console.log("10 -- : " + num5.decrease()());
+console.log("10 -- : " + num5.decrease()());
+console.log("10 reset : " + num5.reset()());
+console.log("10 -- : " + num5.decrease()());
+let num6=newCounter(7)
+console.log("7 -- : " + num6.decrease()());
+console.log("7 set 11  : " + num6.set(11)());
+console.log("11 ++ : "+ num6.increase()())
+
+
+// Task 04*
+// Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
+// и что бы корректно работали следующие вызовы:
+// 1) superSum(0) //0
+// 2) superSum(3)(2)(5)(3) //10
+// 3) superSum(3)(2)(5,3) //10
+// 4) superSum(3)(2,5,3) //10
+// 5) superSum(3)(2,5)(3) //10
+// 6) superSum(3)(2,5)(3,9) //10
+
+// P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+
+const superSum = () => {
+
+}
+
+
+// Task 05
+// решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
+
+// Task 06
+// написать функцию, которая повторяет функционал метода flat массива на всю глубину.
+
+// just a plug
+
+
+export default {};
